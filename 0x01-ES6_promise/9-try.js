a6b1f86794aa9ce615469b0e8f43da7631daeced
Precious Nwaigbo <99683218+Tactical98@users.noreply.github.com>
2022-12-21 23:57:13 +0100

    es6 more

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+/* eslint-disable require-jsdoc */
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (err) {
+    queue.push(`Error: ${err.message}`, 'Guardrail was processed');
+  }
+  return queue;
+}
