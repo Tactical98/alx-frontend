3a41d1e885f3f26ab3230aeedfd90a2a9cc4ca8e
Precious Nwaigbo <99683218+Tactical98@users.noreply.github.com>
2022-12-23 15:20:18 +0100

    classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+	constructor(name, length, students) {
+		this.name = name;
+		this.length = length;
+		this.students = students;
+	}
+
+	// Name
+	get name() {
+		return this._name;
+	}
+
+	set name(value) {
+		if (typeof value !== 'string') {
+			throw new TypeError('Name must be a string');
+		}
+
+		this._name = value;
+	}
+
+	// Length
+	get length() {
+		return this._length;
+	}
+
+	set length(value) {
+		if (typeof value !== 'number') {
+			throw new TypeError('Length must be a number');
+		}
+
+		this._length = value;
+	}
+
+	// Students
+	get students() {
+		return this._students;
+	}
+
+	set students(value) {
+		value.forEach(element => {
+			if (typeof element !== 'string') {
+				throw new TypeError('Students must be an array of strings');
+			}
+		});
+		this._students = value;
+	}
+}
